7096cc33945d1ae51af3005744ec6645d20cc610
harutyunyananahit <harutyunyananahit14@gmail.com>
2018-03-20 17:59:54 +0400

    do game structure

--- game.cpp
+++ game.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 int random_coordinate_X()
@@ -12,19 +13,36 @@ int random_coordinate_Y()
   int y = rand() % 5;
   return y;
 }
+
 int main()
+{
   int path_score = 0;
   int basic_score = 0;
   int blue_bird_x, blue_bird_y;
   int red_bird_x, red_bird_y;
   int white_bird_x, white_bird_y;
-  cout << "Welcome to the game FIND A WHITE BIRD. Here are the rules of the game. You are a hunter BLUE BIRD.
-  You need to find the hidden WHITE BIRDS inputing x(in range from 0 to 10) and y(from 0 to 4) coordinates. If
-  you are lucky you earn 50 points, if not we have a bonus for you! 10 points! But disaster! If you meet RED BIRD,
-  you will loose 100 poins and the game. Now... Good luck!!!   "
+  cout << "Welcome to the game FIND A WHITE BIRD. Here are the rules of the game."
+  <<"You are a hunter BLUE BIRD.You need to find the hidden WHITE BIRDS inputing x(in range from 0 to 10)"
+  <<" and y(from 0 to 4) coordinates. If you are lucky you earn 50 points, if not we have a bonus for you! 10 points!"
+  <<" But disaster! If you meet RED BIRD, you will loose 100 poins and the game. Now... Good luck!!!" << endl << endl;
   do {
-    cout << "Pls input BLUE BIRD'S x and y coordinates:   x:  ";  cin >> blue_bird_x;
-    cout << "y:  "; cin >> blue_bird_y;
+    cout << "Pls input BLUE BIRD'S x and y coordinates:   x:  ";
+    cin >> blue_bird_x;
+    while(cin.fail()) {
+       cout << "Wrong input. Try again." << std::endl;
+       cin.clear();
+       cin.ignore(256,'\n');
+       cout << "x:";
+       cin >> blue_bird_x;
+    }
+    cout << "y:  ";
+    cin >> blue_bird_y;
+    while(std::cin.fail()) {
+       std::cout << "Wrong input. Try again." << std::endl;
+       std::cin.clear();
+       std::cin.ignore(256,'\n');
+       std::cin >> blue_bird_y;
+    }
     cout << endl;
     red_bird_x = random_coordinate_X();
     red_bird_y = random_coordinate_Y();
@@ -42,9 +60,9 @@ int main()
       cout << "Oops! Red bird -_-" << endl << "Your game is over and your FINAL SCORE is " << path_score + basic_score - 100;
       break;
     }
-    cout << "Don't surrender. Try again! Find a white bird and earn 50 points!" << endl;
+    cout << "Don't surrender. Try again! Find a WHITE BIRD and earn 50 points!" << endl;
   }
   while(true);
-  
+
   return 0;
 }
